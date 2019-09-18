a8d7bc714bc9d9ca8e977993dc4dcb72a7e416c2
Kyungmin <kmlee0206@gmail.com>
2019-09-18 09:21:54 -0400

    cleanup

--- src/test/java/com/revature/service/CustomerServiceTest.java
+++ src/test/java/com/revature/service/CustomerServiceTest.java
@@ -30,24 +30,39 @@ public class CustomerServiceTest {
 //		double testValue = Transactions.viewBalance(username);
 //		assertEquals(100, testValue);
 		assertTrue(Transactions.viewBalance(username) == 100);
-		
 	}
 	
 	@Test
-	public void deposit() {
-		Transactions.deposit(50, username);
-//		double testVal = Transactions.viewBalance(username);
-//		assertEquals(150, testVal);
-		assertTrue(Transactions.viewBalance(username) == 500);
+	public void pullName() {
+//		double testValue = Transactions.viewBalance(username);
+//		assertEquals(100, testValue);
+		assertTrue(Transactions.pullName(username) == "Dumbo");
 	}
 	
 	@Test
-	public void withdraw() {
-		Transactions.withdraw(25, username);
-//		double testVal = Transactions.viewBalance(username);
-//		assertEquals(75, testVal);
-		assertTrue(Transactions.viewBalance(username) == 500);
+	public void pullUserpasswd() {
+//		double testValue = Transactions.viewBalance(username);
+//		assertEquals(100, testValue);
+		assertTrue(Transactions.pullUserpwd(username) == "passtest");
 	}
+	
+	
+	
+//	@Test
+//	public void pullName() {
+//		Transactions.deposit(50, username);
+////		double testVal = Transactions.viewBalance(username);
+////		assertEquals(150, testVal);
+//		assertTrue(Transactions.viewBalance(username) == 100);
+//	}
+	
+//	@Test
+//	public void withdraw() {
+//		Transactions.withdraw(25, username);
+////		double testVal = Transactions.viewBalance(username);
+////		assertEquals(75, testVal);
+//		assertTrue(Transactions.viewBalance(username) == 100);
+//	}
 	
 	
 
